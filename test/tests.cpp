edf61c24f29c94b654c1ad1c6b646b76a34d838c
joaoLouceiro <jrlouceiro@live.com>
2025-09-23 16:03:13 +0100

    Add process::launch tests
    - Test for successful launch working
    - Failing when testing nonexisting program

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,5 +1,26 @@
 #include <catch2/catch_test_macros.hpp>
+#include <libjdb/error.hpp>
+#include <libjdb/process.hpp>
+#include <signal.h>
+#include <sys/types.h>
 
-TEST_CASE("validate environment") {
-    REQUIRE(true);
+using namespace jdb;
+
+namespace {
+bool process_exists(pid_t pid)
+{
+    auto ret = kill(pid, 0);
+    return ret != -1 and errno != ESRCH;
+}
+}
+
+TEST_CASE("process::launch success", "[process]")
+{
+    auto proc = process::launch("yes");
+    REQUIRE(process_exists(proc->pid()));
+}
+
+TEST_CASE("process::launch no such program", "[process]")
+{
+    REQUIRE_THROWS_AS(process::launch("there_is_no_such_program_here"), error);
 }
